81cafe1245997cfc5812ddc2f5db95a21007bd18
khalidshabbirvoltro <khalid.shabbir@voltro>
2023-08-01 07:02:46 -0700

    complete table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,28 +1,20 @@
 <script setup>
 
-const props = defineProps({
-    title: String,
-    headerclasses: String,
-    columns: Array,
-    rows: Array,
-    tableStripe: Boolean,
-    loading: Boolean,
-    hidepaggination: Boolean,
-    rowPerPage: Number
+const props = defineProps(['rows','columns','pagination', 'onReuestFunction','loading','color','title','headerclasses','tablestripe','flat','hidepaggination'])
 
-});
+
+
 </script>
 
 <template>
-    <q-table card-class="" :table-header-class="headerclasses" flat bordered :title="title" :rows="rows" :columns="columns"
-        row-key="name" :class="{ 'table-stripe': tableStripe }" :loading="loading" :hide-pagination="hidepaggination" 
-        :rows-per-page-options="rowPerPage">
-        <template v-slot:loading>
+   <q-table v-model:pagination="props.pagination"   :title="props.title" :table-header-class="props.headerclasses" :class="{ 'table-stripe': props.tablestripe }" :hide-pagination="props.hidepaggination" :loading="loading" :rows="props.rows" :columns="props.columns"
+             :flat="props.flat"
+            @request="props.onReuestFunction" >
+            <template v-slot:loading>
             <q-inner-loading showing color="primary" />
           
         </template>
-        
-    </q-table>
+            </q-table>
 </template>
 
 <style scoped>
--- src/pages/Table.vue
+++ src/pages/Table.vue
@@ -1,58 +1,58 @@
 <script setup>
-import Tablepage from '../components/Table.vue'
+import TableComponent from '../components/Table.vue'
 import axios from 'axios'
-import {ref} from 'vue'
+import { ref,reactive } from 'vue'
 const columns = [
-      { name: 'name', label: 'Name', field: 'name', align: 'left', sortable: true },
-      { name: 'age', label: 'Age', field: 'age', align: 'center' },
-      { name: 'email', label: 'Email', field: 'email' }
-    ]
+  { name: 'name', label: 'Name', field: 'name', align: 'left', sortable: true },
+  { name: 'age', label: 'Age', field: 'age', align: 'center',  sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'email', label: 'Email', field: 'email' }
+]
 const dogs = ref([])
-const fetchDogs = (page =0,limit=15 ) => { // we can specify the "page" to jump to
+
+const pagination = reactive({
+      page: 1,
+      rowsPerPage: 10,
+      rowsNumber: 1
+    })
+const fetchDogs = (page = 0, limit = 10) => { 
+     
+  return axios.get('https://table.quasarcomponents.com/dogs', {
+    params: { page: page, limit: limit } 
+  })
+    .then(response => {
+      
+      dogs.value = response.data.data
+    
+      const meta = response.data.meta
+      pagination.page = meta.current_page
+      pagination.rowsPerPage = meta.per_page
+      pagination.rowsNumber = meta.total
+    })
+    .finally(() => {
    
-      return axios.get('https://table.quasarcomponents.com/dogs', {
-        params: { page: page , limit:limit} // here, we tell the api what "page" to jump to
-      })
-        .then(response => {
-          dogs.value = response.data.data
-         
-          const meta = response.data.meta
+    })
+}
 
+const onRequest = (props) => {
+  fetchDogs(props.pagination.page, props.pagination.rowsPerPage)
+}
 
-          pagination.value.page = meta.current_page
-          pagination.value.rowsPerPage = meta.per_page
-          pagination.value.rowsNumber = meta.total
-        })
-        .finally(() => {
-          loading.value = false
-        })
-    }
-
-    const onRequest = (props) => {
-      fetchDogs(props.pagination.page,props.pagination.rowsPerPage)
-    }
-
-    // The initial fetch
-    fetchDogs()
+fetchDogs()
 
 
 </script>
 <template>
   <q-layout>
 
-  <q-page-container>
-    <q-page class="q-pa-xl">
-    <Tablepage title="Latest Requests" :columns="columns" :rows="dogs" headerclasses="bg-primary text-white"  :tableStripe="true" :loading="false" :hidepaggination="false"
-    :rowPerPage="[10,15,20,30]"
-    />
-    </q-page>
-  </q-page-container>
+    <q-page-container>
+      <q-page class="q-pa-xl">
+        <TableComponent title="Table" :rows="dogs" :columns="columns" headerclasses="bg-dark text-white" :pagination="pagination" :onReuestFunction="onRequest" :loading="false" :tablestripe="true" :flat="flat" :hidepaggination="true"/>
+      </q-page>
+    </q-page-container>
 
-</q-layout>
+  </q-layout>
 </template>
 
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
